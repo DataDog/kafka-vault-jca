682a879eb0875ec477986699f22806b2e1f570d2
Yann Schwartz <abolibibelot@gmail.com>
2020-09-10 16:30:24 +0200

    Add proper logging for 4xx vault errors

--- src/main/java/com/ultimatesoftware/dataplatform/vaultjca/services/DefaultVaultService.java
+++ src/main/java/com/ultimatesoftware/dataplatform/vaultjca/services/DefaultVaultService.java
@@ -3,10 +3,16 @@ package com.ultimatesoftware.dataplatform.vaultjca.services;
 import com.bettercloud.vault.Vault;
 import com.bettercloud.vault.VaultConfig;
 import com.bettercloud.vault.VaultException;
+
+import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Map;
 
 import com.bettercloud.vault.response.AuthResponse;
+import com.bettercloud.vault.response.LogicalResponse;
+import com.bettercloud.vault.response.VaultResponse;
+import com.bettercloud.vault.rest.Rest;
+import com.bettercloud.vault.rest.RestResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,7 +42,13 @@ public class DefaultVaultService implements VaultService {
   @Override
   public Map<String, String> getSecret(String path) {
     try {
-      return vault.logical().read(path).getData();
+      LogicalResponse resp = vault.logical().read(path);
+      RestResponse rest = resp.getRestResponse();
+      if (rest.getStatus() != 200){ // Status codes in the 4xx range are not treated as exceptions by the driver...
+        log.warn("Error contacting vault. Status: {} Message was: {}", rest.getStatus(), new String(rest.getBody(), StandardCharsets.UTF_8));
+      }
+
+      return resp.getData();
     } catch (VaultException e) {
       if (e.getHttpStatusCode() == 404) {
         return Collections.EMPTY_MAP;
